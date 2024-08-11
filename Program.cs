0b5f5b686fa952a3efb69e26050f052cf0b1fcd7
KIRS1K <ilya.kirsnov@bk.ru>
2024-08-11 11:33:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ReadMyFile
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string filePath = @"D:\1\Новая папка\";
+            if (!File.Exists(filePath))
+            {
+                string[] allfiles = Directory.GetFiles(filePath, "", SearchOption.TopDirectoryOnly);
+                for (int i = 0; i < allfiles.Length; i++) 
+                {
+                    string plase = allfiles[i];
+                    Console.WriteLine(plase);
+                    File.Delete(plase);
+                }
+                Console.WriteLine("+");
+
+            }
+            else { Console.WriteLine("-"); }
+        }
+    }
+}
